2d4b7182ae47ea27e2d23ee2939f3b7086606b12
nerdgrass <arboerstler@gmail.com>
2015-09-07 12:25:16 -0400

    adds mustache, now serves template for index

--- src/nerdgrass/controllers/posts.clj
+++ src/nerdgrass/controllers/posts.clj
@@ -1,3 +1,13 @@
-(ns nerdgrass.controllers.posts)
+(ns nerdgrass.controllers.posts
+  (:require
+      [clostache.parser :as clostache]))
 
-(defn index [] "Hello, from index")
+(defn read-template [template-name]
+  (slurp (clojure.java.io/resource
+    (str "views/posts/" template-name ".mustache"))))
+
+(defn render-template [template-file params]
+  (clostache/render (read-template template-file) params))
+
+(defn index []
+  (render-template "index" {:name "clojure developer"}))
